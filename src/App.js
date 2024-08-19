16ce130a062777f7f85a0cb652bb6590719cd7df
Yocanantham <yocanantham@gmail.com>
2024-08-19 18:11:36 +0530

    feat: a comprehensive understanding map in react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList";
 import "./api";
 import searchImages from "./api";
 
 function App() {
-    searchImages();
-    const handleSearch = (term) => {
-        console.log("Communication made", term);
+    const [images, setImages] = useState([]);
+    const handleSearch = async (term) => {
+        const result = await searchImages(term);
+        setImages(result);
+        console.log('A search was made for : "', term, '"');
     };
-
     return (
         <div>
             <SearchBar onSubmit={handleSearch} />
-            <ImageList />
+            <ImageList images={images} />
         </div>
     );
 }
--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get("https://api.unsplash.com/photos", {
         headers: {
             Authorization:
                 "Client-ID vdvXDe1-inKmdC7DOTvymrw_57xcuY41EwmPejr6Jj0",
         },
         params: {
-            query: "cars",
+            query: term,
         },
     });
-
     return response.data;
 };
 
